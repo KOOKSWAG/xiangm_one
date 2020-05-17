eebdf6074f8350f18377b0c55e3e22a84a89283c
KOOKSWAG <1578925156@qq.com>
2020-05-17 18:14:34 +0800

    添加布局

--- src/page/home/index/Two.js
+++ src/page/home/index/Two.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react'
+import { Form, Input, InputNumber, Button } from 'antd';
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+  
+  const validateMessages = {
+    required: '${label} is required!',
+    types: {
+      email: '${label} is not validate email!',
+      number: '${label} is not a validate number!',
+    },
+    number: {
+      range: '${label} must be between ${min} and ${max}',
+    },
+  };
 
 export default class Two extends Component {
+    onFinish = values => {
+        console.log(values);
+      };
     render() {
         return (
-            <div>
-                345345
-            </div>
+            <Form {...layout} name="nest-messages" onFinish={this.onFinish} validateMessages={validateMessages}>
+                <Form.Item name={['user', 'name']} label="用户名" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'age']} label="年龄" rules={[{ type: 'number', min: 0, max: 99 }]}>
+                    <InputNumber />
+                </Form.Item>
+                <Form.Item name={['user', 'gender']} label="标题" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'msg']} label="价格" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'hospital']} label="信息" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                    <Button type="primary" htmlType="submit">
+                        添加
+                    </Button>
+                </Form.Item>
+            </Form>
         )
     }
 }
